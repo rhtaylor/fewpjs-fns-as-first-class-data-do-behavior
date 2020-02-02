b546beff496b1305603218cbc066b46a7ad59629
Randy <randyisbusy@gmail.com>
2020-02-02 12:30:15 -0700

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,37 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+/* Write your implementation of displayMessage() */  
+
+function displayMessage(arg){ 
+  let location = document.querySelector("#greeting")
+  
+    location.innerText = greet(arg) 
+  
+} 
+
+function greet(time){ 
+     let prep = time.split(":") 
+     let rough = prep.slice(0,1) 
+     
+     let polish = rough.join()
+    
+ let tTime = parseInt(polish)  
+ console.log(tTime)
+ 
+ let phrase = time
+ if (tTime > 17) { 
+   
+ phrase = "Good Evening"; 
+ } else if (tTime <= 17 && tTime >= 12) { 
+   
+ phrase =  "Good Afternoon";
+ } else if (tTime < 12 )   {
+   phrase = "Good Morning" ; 
+ } else if (tTime == NaN) {
+   return phrase
+
+ } 
+ 
+ return phrase
+}
